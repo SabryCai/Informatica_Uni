33997d86d5a6ac7631d37fcc617a2bafd97ce092
Sabry <sabr.caiazza@studenti.unina.it>
2026-04-15 23:16:51 +0200

    Add files via upload

---
+++ 2° Anno/ 1° Semestre/Algoritmi e Progettazione Strutture Dati/Libreria (Appello di Febbraio)/apsd/interfaces/containers/collections/Chain.java
@@ -0,0 +1,40 @@
+package apsd.interfaces.containers.collections;
+
+import apsd.classes.utilities.Natural;
+import apsd.interfaces.containers.sequences.RemovableAtSequence;
+
+public interface Chain<Data> extends Set<Data>, RemovableAtSequence<Data> {
+  
+  //Insert if absent
+  default boolean InsertIfAbsent(Data dat) {
+    if (!Exists(dat)) {
+      return Insert(dat);
+    }
+    return false;
+  }
+  
+  //Remove all occurrences
+  default void RemoveOccurrences(Data dat) {
+    while (Remove(dat)) {
+    }
+  }
+  
+  //SubChain
+  default Chain<Data> SubChain(Natural from, Natural to) {
+    return (Chain<Data>) SubSequence(from, to);
+  }
+  
+  @Override
+  default Natural Search(Data dat) {
+      var itr = FIterator();
+      long index = 0;
+      while (itr.IsValid()) {
+        Data current = itr.DataNNext();
+        if ((current == null && dat == null) || (current != null && current.equals(dat))) {
+          return Natural.Of(index);
+        }
+        index++;
+      }
+    return Size();
+  }
+}
---
+++ 2° Anno/ 1° Semestre/Algoritmi e Progettazione Strutture Dati/Libreria (Appello di Febbraio)/apsd/interfaces/containers/collections/List.java
@@ -0,0 +1,19 @@
+package apsd.interfaces.containers.collections;
+
+import apsd.classes.utilities.Natural;
+import apsd.interfaces.containers.sequences.InsertableAtSequence;
+import apsd.interfaces.containers.sequences.MutableSequence;
+
+public interface List<Data> extends MutableSequence<Data>, InsertableAtSequence<Data>, Chain<Data> {
+  
+  //SubList
+  default List<Data> SubList(Natural from, Natural to) {
+    return (List<Data>) SubSequence(from, to);
+  }
+  
+  @Override
+  default boolean Insert(Data dat) {
+    InsertFirst(dat);
+    return true;
+  }
+}
